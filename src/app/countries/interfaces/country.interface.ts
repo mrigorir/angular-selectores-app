392eb93b53d5460e30b8f396fe5ff27dff026c5e
Marco Parra <marcoparra311@gmail.com>
2024-07-17 12:03:21 -0400

    Create service to get country depending on continent selecteted

--- src/app/countries/interfaces/country.interface.ts
+++ src/app/countries/interfaces/country.interface.ts
@@ -1,39 +1,52 @@
+export enum Region {
+  Africa = 'Africa',
+  Americas = 'Americas',
+  Asia = 'Asia',
+  Europa = 'Europa',
+  Oceania = 'Oceania',
+}
+
+export interface SmallCountry {
+  name: string;
+  cca3: string;
+  borders: string[];
+}
 export interface Country {
-  name:         Name;
-  tld?:         string[];
-  cca2:         string;
-  ccn3?:        string;
-  cca3:         string;
+  name: Name;
+  tld?: string[];
+  cca2: string;
+  ccn3?: string;
+  cca3: string;
   independent?: boolean;
-  status:       Status;
-  unMember:     boolean;
-  currencies:   Currencies;
-  idd:          Idd;
-  capital:      string[];
+  status: Status;
+  unMember: boolean;
+  currencies: Currencies;
+  idd: Idd;
+  capital: string[];
   altSpellings: string[];
-  region:       Region;
-  subregion:    Subregion;
-  languages:    { [key: string]: string };
+  region: Region;
+  subregion: Subregion;
+  languages: { [key: string]: string };
   translations: { [key: string]: Translation };
-  latlng:       number[];
-  landlocked:   boolean;
-  borders?:     string[];
-  area:         number;
-  demonyms:     Demonyms;
-  flag:         string;
-  maps:         Maps;
-  population:   number;
-  fifa?:        string;
-  car:          Car;
-  timezones:    string[];
-  continents:   string[];
-  flags:        Flags;
-  coatOfArms:   CoatOfArms;
-  startOfWeek:  StartOfWeek;
-  capitalInfo:  CapitalInfo;
-  cioc?:        string;
-  gini?:        { [key: string]: number };
-  postalCode?:  PostalCode;
+  latlng: number[];
+  landlocked: boolean;
+  borders?: string[];
+  area: number;
+  demonyms: Demonyms;
+  flag: string;
+  maps: Maps;
+  population: number;
+  fifa?: string;
+  car: Car;
+  timezones: string[];
+  continents: string[];
+  flags: Flags;
+  coatOfArms: CoatOfArms;
+  startOfWeek: StartOfWeek;
+  capitalInfo: CapitalInfo;
+  cioc?: string;
+  gini?: { [key: string]: number };
+  postalCode?: PostalCode;
 }
 
 export interface CapitalInfo {
@@ -42,24 +55,18 @@ export interface CapitalInfo {
 
 export interface Car {
   signs: string[];
-  side:  Side;
+  side: Side;
 }
 
 export enum Side {
-  Left = "left",
-  Right = "right",
+  Left = 'left',
+  Right = 'right',
 }
 
 export interface CoatOfArms {
   png?: string;
   svg?: string;
 }
-
-export enum Region {
-  Asia = "Asia",
-  Europe = "Europe",
-}
-
 export interface Currencies {
   GIP?: All;
   ISK?: All;
@@ -89,7 +96,7 @@ export interface Currencies {
 }
 
 export interface All {
-  name:   string;
+  name: string;
   symbol: string;
 }
 
@@ -98,7 +105,7 @@ export interface BAM {
 }
 
 export interface Demonyms {
-  eng:  Eng;
+  eng: Eng;
   fra?: Eng;
 }
 
@@ -108,51 +115,51 @@ export interface Eng {
 }
 
 export interface Flags {
-  png:  string;
-  svg:  string;
+  png: string;
+  svg: string;
   alt?: string;
 }
 
 export interface Idd {
-  root:     string;
+  root: string;
   suffixes: string[];
 }
 
 export interface Maps {
-  googleMaps:     string;
+  googleMaps: string;
   openStreetMaps: string;
 }
 
 export interface Name {
-  common:     string;
-  official:   string;
+  common: string;
+  official: string;
   nativeName: { [key: string]: Translation };
 }
 
 export interface Translation {
   official: string;
-  common:   string;
+  common: string;
 }
 
 export interface PostalCode {
   format: string;
-  regex:  string;
+  regex: string;
 }
 
 export enum StartOfWeek {
-  Monday = "monday",
+  Monday = 'monday',
 }
 
 export enum Status {
-  OfficiallyAssigned = "officially-assigned",
-  UserAssigned = "user-assigned",
+  OfficiallyAssigned = 'officially-assigned',
+  UserAssigned = 'user-assigned',
 }
 
 export enum Subregion {
-  CentralEurope = "Central Europe",
-  EasternEurope = "Eastern Europe",
-  NorthernEurope = "Northern Europe",
-  SoutheastEurope = "Southeast Europe",
-  SouthernEurope = "Southern Europe",
-  WesternEurope = "Western Europe",
+  CentralEurope = 'Central Europe',
+  EasternEurope = 'Eastern Europe',
+  NorthernEurope = 'Northern Europe',
+  SoutheastEurope = 'Southeast Europe',
+  SouthernEurope = 'Southern Europe',
+  WesternEurope = 'Western Europe',
 }
--- src/app/countries/pages/selector-page/selector-page.component.ts
+++ src/app/countries/pages/selector-page/selector-page.component.ts
@@ -1,18 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CountriesService } from '../../services/countries.service';
+import { Region, SmallCountry } from '../../interfaces/country.interface';
+import { delay, switchMap, tap } from 'rxjs';
 
 @Component({
   selector: 'app-selector-page',
   templateUrl: './selector-page.component.html',
   styles: ``,
 })
-export class SelectorPageComponent {
+export class SelectorPageComponent implements OnInit {
+  constructor(
+    private fb: FormBuilder,
+    private countriesService: CountriesService
+  ) {}
 
-  constructor(private fb: FormBuilder) {}
+  countriesByRegion: SmallCountry[] = [];
+  loader: boolean = false;
 
   myForm: FormGroup = this.fb.group({
     region: ['', Validators.required],
     country: ['', Validators.required],
-    borders: ['', Validators.required],
+    border: ['', Validators.required],
   });
+
+  ngOnInit(): void {
+    this.onRegionChange();
+  }
+
+  get regions(): Region[] {
+    return this.countriesService.regions;
+  }
+
+  onRegionChange(): void {
+    this.myForm
+    .get('region')!
+    .valueChanges.pipe(
+      tap(() => this.myForm.get('country')?.setValue('')),
+        switchMap((region) =>
+          this.countriesService.getCountriesByRegion(region)
+        )
+      )
+      .subscribe((countries) => {
+        this.loader = true;
+        this.countriesByRegion = countries;
+        this.loader = false;
+      });
+  }
 }
